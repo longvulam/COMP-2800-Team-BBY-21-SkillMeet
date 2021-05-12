e58b4bb9ac7d4fc141db64f4f67ad14f02dd9fb3
Lam Long Vu <longvulam91@gmail.com>
2021-05-13 00:43:33 +0200

    Added proxy into package.json for client fetch calls.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function fetchCallExample() {
+    fetch("/fetchExampleApi")
+        .then(async response => console.log(await response.json()));
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>Add your page components here,</div>
-        <div>we'll merge them later.</div>
-      </header>
-    </div>
-  );
+    // fetchCallExample();
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>Add your page components here,</div>
+                <div>we'll merge them later.</div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
