7eec882dd1773fad3262f0ef3ffb770982b077f9
Lam Long Vu <longvulam91@gmail.com>
2021-05-18 14:28:46 -0700

    Fixed App.test.js.

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,8 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/SkillMeet/i,{
+      selector: "div"
+  });
   expect(linkElement).toBeInTheDocument();
 });
