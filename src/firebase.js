2104c03737e7638086e67e03c6285331db78ccb4
Lam Long Vu <longvulam91@gmail.com>
2021-05-18 13:42:37 -0700

    Added get user profile data, and example code to access it.

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,8 @@ import firebase from 'firebase';
 
 
 // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
     apiKey: "AIzaSyCbw_obCsHkJLB4ZMMAzzTkqj4K2mH3Rx4",
     authDomain: "skillmeet-4c737.firebaseapp.com",
     projectId: "skillmeet-4c737",
@@ -11,14 +11,54 @@ import firebase from 'firebase';
     messagingSenderId: "383981362110",
     appId: "1:383981362110:web:51ec2a385d20db147ab7ef",
     measurementId: "G-NE6BPLMNP9"
-  };
+};
 
- const fire = firebase.initializeApp(firebaseConfig);
+const fire = firebase.initializeApp(firebaseConfig);
 
 
 export default fire;
- export const auth = firebase.auth();
- export const db = firebase.firestore();
+export const auth = firebase.auth();
+export const db = firebase.firestore();
 
-  // Initialize Firebase
-//   firebase.analytics();
+let isRetrievingData = false;
+
+async function getProfileDataFromDb(uid) {
+
+    if (isRetrievingData) return;
+    isRetrievingData = true;
+
+    const ref = db.collection('users').doc(uid);
+
+    const data = await Promise.all([
+        ref.get().then(doc => doc.data()),
+        ref.collection('Skills')
+        .get().then(querySnapshot => {
+            const arr = [];
+            querySnapshot.forEach(doc => arr.push(doc.data()));
+            return arr;
+        }),
+    ]);
+
+    const profileData = data[0];
+    profileData.skills = data[1];
+    isRetrievingData = false;
+    return profileData;
+}
+
+export function getCurrentUserDataAsync() {
+    return new Promise((resolve, reject) =>
+        auth.onAuthStateChanged((user) => {
+            if (!user) return;
+
+            let userData = getProfileDataFromDb(user.uid);
+            resolve(userData);
+        })
+    );
+}
+
+async function retrieveUserProfileDataExample(){
+    const userData = await getCurrentUserDataAsync();
+    console.log(userData);
+}
+
+// retrieveUserProfileDataExample();
