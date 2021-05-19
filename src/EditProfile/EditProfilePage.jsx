dd1da89eb9e411e9cb6a717c6e517dda349a331a
Lam Long Vu <longvulam91@gmail.com>
2021-05-18 21:06:38 -0700

    Added editing state on EditProfile page.

--- src/EditProfile/EditProfilePage.jsx
+++ src/EditProfile/EditProfilePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Avatar from '@material-ui/core/Avatar';
@@ -14,196 +14,173 @@ import EditableSkill from './editProfileComponents/EditableSkill';
 import IconButton from '@material-ui/core/IconButton';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 
-import firebase, { db } from '../firebase';
+import { getCurrentUserDataAsync } from '../firebase';
 
-const useStyles = makeStyles((theme) => ({
-  editIcon: {
-    marginRight: '3vw',
-    marginTop: '2vw',
-    height: '3em',
-    width: '3em',
-  },
-  editWrap: {
-    width: '100vw',
-    display: 'flex',
-    justifyContent: 'flex-end',
-  },
-  avatarWrap: {
-    width: '100vw',
-    height: '5em',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  avatar: {
-    height: '4.5em',
-    width: '4.5em',
-  },
-  infoWrap: {
+async function submitChanges(profile) {
+    console.log(profile);
+}
 
-  },
-}));
-
+async function addSkill(profile){
+    
+}
 
 export default function Profile() {
-  const classes = useStyles();
-  const [skills, setSkills] = useState([]);
-  getProfileDataAsync(setSkills);
-  const data = sampleSkilldata;
-  const [editable, setEditable] = useState(false);
+    const classes = useStyles();
+    const [userProfile, setUserProfile] = useState({
+        firstName: "",
+        lastName: "",
+        location: "",
+        bio: "",
+        skills: [],
+    });
+    useEffect(() => getCurrentUserDataAsync().then(setUserProfile), []);
 
-  return (
-    <div style={{
-      width:'100vw',
-      height:'calc(100vh - 4em)',
-      overflowY:'scroll',
-      overflowX:'hidden',
-    }}>
-       <div className={classes.editWrap}> 
-        <CancelButton
-          style={{marginRight: '6vw',
-          marginTop: '2vw',
-          height: '2.5em',
-          width: '2.5em',}}
-        />
-        <SaveButton
-          editable={editable} 
-          style={{marginRight: '4vw',
-          marginTop: '2vw',
-          height: '2.5em',
-          width: '2.5em',}}
-        />
-      </div>
-    
-      <div className={classes.avatarWrap}>
-       <Avatar 
-        alt="C" 
-        src="/static/images/avatar/1.jpg" 
-        className={classes.avatar} />
-      </div>
-  
-      <Grid container direction="column" spacing = {1}
-        style={{
-          margin:'auto',
-          marginTop: '1vh',
-          alignItems: 'center',
+    return (
+        <div style={{
+            width: '100vw',
+            height: 'calc(100vh - 4em)',
+            overflowY: 'scroll',
+            overflowX: 'hidden',
         }}>
-        <Grid item xs={12}>
-          <InputBase
-            defaultValue="Chris"
-            readOnly= {!editable}
-            inputProps={{ 
-              'aria-label': 'naked',
-              style: {
-                textAlign: 'center',
-                border:'none',
-              }
-           }}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <InputBase
-            readOnly= {!editable}
-            defaultValue="That Expensive Hotel"
-            inputProps={{ 
-              'aria-label': 'naked',
-              style: {
-                textAlign: 'center',
-                border:'none',
-              }
-           }}
-          />
-        </Grid>
-      </Grid>
-  
-      <Grid container direction="column" spacing = {1}
-        style={{
-          margin: 'auto',
-          marginTop: '2vh',
-          width: '95vw',
-          alignItems: 'center',
-        }}>
-        {data.map(accordion => {
-          const { skillName, skillLevel, skillDescription } = accordion;
-          return (
-            <Grid item xs={12} style={{
-              width:'100%',
-            }}>
-              <EditableSkill
-                skillName={skillName}
-                skillLevel={skillLevel} 
-                skillDescription={skillDescription}
-                editable={editable}
-              />
+            <div className={classes.editWrap}>
+                <CancelButton
+                    style={{
+                        marginRight: '6vw',
+                        marginTop: '2vw',
+                        height: '2.5em',
+                        width: '2.5em',
+                    }}
+                />
+                <SaveButton
+                    onClick={(event)=> submitChanges(userProfile)}
+                    editable={true}
+                    style={{
+                        marginRight: '4vw',
+                        marginTop: '2vw',
+                        height: '2.5em',
+                        width: '2.5em',
+                    }}
+                />
+            </div>
+
+            <div className={classes.avatarWrap}>
+                <Avatar
+                    alt="C"
+                    src="/static/images/avatar/1.jpg"
+                    className={classes.avatar} />
+            </div>
+
+            <Grid container direction="column" spacing={1}
+                style={{
+                    margin: 'auto',
+                    marginTop: '1vh',
+                    alignItems: 'center',
+                }}>
+                <Grid item xs={12}>
+                    <InputBase
+                        value={userProfile.firstName + " " + userProfile.lastName}
+                        readOnly={true}
+                        inputProps={{
+                            'aria-label': 'naked',
+                            style: {
+                                textAlign: 'center',
+                                border: 'none',
+                            }
+                        }}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <InputBase
+                        readOnly={false}
+                        value={userProfile.location}
+                        inputProps={{
+                            'aria-label': 'naked',
+                            style: {
+                                textAlign: 'center',
+                                border: 'none',
+                            }
+                        }}
+                    />
+                </Grid>
             </Grid>
-          );
-        })}
-        <Grid item xs={12}
-          style={{
-            width: '100%',
-            display:'flex', 
-            alignItems:'center',
-            justifyContent:'center',
-          }}>
-          <IconButton>
-            <AddCircleIcon
-              style={{
-                width:'1.5em',
-                height:'1.5em',
-              }}
-            />
-          </IconButton>
-          
-        </Grid>
-        <Grid item xs={12}
-          style={{
-            width: '100%'
-          }}>
-          <ProfileBio editable={editable}/>
-        </Grid>
-      </Grid>
-    
-    </div>
-  );
+
+            <Grid container direction="column" spacing={1}
+                style={{
+                    margin: 'auto',
+                    marginTop: '2vh',
+                    width: '95vw',
+                    alignItems: 'center',
+                }}>
+                {userProfile.skills.map(skill => {
+                    return (
+                        <Grid key={skill.skillName} item xs={12} style={{
+                            width: '100%',
+                        }}>
+                            <EditableSkill
+                                data={skill}
+                                skillsList={userProfile.skills}
+                                changeState={setUserProfile}
+                            />
+                        </Grid>
+                    );
+                })}
+                <Grid item xs={12}
+                    style={{
+                        width: '100%',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                    <IconButton 
+                    onClick={() => addSkill(userProfile)}>
+                        <AddCircleIcon
+                            style={{
+                                width: '1.5em',
+                                height: '1.5em',
+                            }}
+                        />
+                    </IconButton>
+
+                </Grid>
+                <Grid item xs={12}
+                    style={{
+                        width: '100%'
+                    }}>
+                    <ProfileBio 
+                    bio={userProfile.bio}
+                    changeState={setUserProfile}
+                    editable={true} />
+                </Grid>
+            </Grid>
+
+        </div>
+    );
 }
 
-const sampleSkilldata = [
-    {
-      skillName:'Python',
-      skillLevel:'Expert',
-      skillDescription:'SkillDescription',
+const useStyles = makeStyles((theme) => ({
+    editIcon: {
+        marginRight: '3vw',
+        marginTop: '2vw',
+        height: '3em',
+        width: '3em',
     },
-    {
-      skillName:'Java',
-      skillLevel:'Beginner',
-      skillDescription:'SkillDescription',
+    editWrap: {
+        width: '100vw',
+        display: 'flex',
+        justifyContent: 'flex-end',
     },
-    {
-      skillName:'SQL',
-      skillLevel:'Intermediate',
-      skillDescription:'SkillDescription',
+    avatarWrap: {
+        width: '100vw',
+        height: '5em',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
-    {
-      skillName:'React',
-      skillLevel:'Beginner',
-      skillDescription:'SkillDescription',
+    avatar: {
+        height: '4.5em',
+        width: '4.5em',
     },
-]
+    infoWrap: {
 
-async function getProfileDataAsync(setDataCallback) {
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-            
-            // setDataCallback(user);
-          console.log(user);
-        } else {
-            // redirect
-        }
-      });
-  
-  const data = {
-      
-  };
-
-  return (data);
-}
+    },
+}));
--- src/ProfilePage/profileComponents/ProfileBio.jsx
+++ src/ProfilePage/profileComponents/ProfileBio.jsx
@@ -4,28 +4,42 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 
+const defaultBioText = `Hi I'm Chris, I'm an excellent instructor and I just love group 21!! Click the Edit Button on the top right to edit any of these fields. Notice that the Skills Accordion will stay open during editing. When the editing is done, they will be able to open and close again.`;
+
 export default function ProfileBio(props) {
-  const defaultBioText = `Hi I'm Chris, I'm an excellent instructor and I just love group 21!! Click the Edit Button on the top right to edit any of these fields. Notice that the Skills Accordion will stay open during editing. When the editing is done, they will be able to open and close again.`
-  const bioInfo = props.bio? props.bio : defaultBioText;
-  return (
-    <Paper 
-    elevation={2}
-    style={{
-      width: '100%',
-      display:'flex',
-      alignItems:'center',
-      justifyContent:'center',
-    }}
-    >
-      <InputBase 
-      readOnly={!props.editable}
-      multiline
-      defaultValue={bioInfo}
-      style={{
-        textAlign: 'center',
-        width:'92.5%',
-      }}>
-      </InputBase>
-    </Paper>
-  );
+    const { bio, editable, changeState } = props;
+
+    const bioInfo = bio ? bio : defaultBioText;
+
+    async function updateBio(newValue) {
+        changeState(previousValues => {
+            return {
+                ...previousValues,
+                bio: newValue
+            }
+        });
+    }
+
+    return (
+        <Paper
+            elevation={2}
+            style={{
+                width: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <InputBase
+                value={bioInfo}
+                onChange={(event) => updateBio(event.target.value)}
+                readOnly={!editable}
+                multiline
+                style={{
+                    textAlign: 'center',
+                    width: '92.5%',
+                }}>
+            </InputBase>
+        </Paper>
+    );
 }
