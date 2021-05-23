8c0b4be1b5a063f6ca65246313e33141cea4076c
Lam Long Vu <longvulam91@gmail.com>
2021-05-22 23:36:05 -0700

    Updated chatRoomCard WIP with styling.

--- src/chat/chatPageComponents/chatRoomCard.jsx
+++ src/chat/chatPageComponents/chatRoomCard.jsx
@@ -1,12 +1,38 @@
-import Avatar from '@material-ui/core/Avatar';
+import { Avatar, makeStyles, Paper } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
-export default function ChatRoomCard (props) {
-  return (
-    <>
-    <div>
-        ChatRoomCard
-    </div>
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+    },
+    avatar : {
+        height:'2.5em',
+        width:'2.5em',
+        marginRight:'1em',
+      },
+}));
 
-    </>
-  )
+async function navigateToProfile(event) {
+    
+}
+
+export default function ChatRoomCard(props) {
+    const { room } = props;
+
+    const classes = useStyles();
+const location = {
+    pathname: "./chatroom/" + room.roomId,
+    state: { chatRoomName: room.name }
+}
+    return (
+        <Link to={location} >
+            <Paper className={classes.paper} >
+                <Avatar className={classes.avatar}/>
+                <div>{room.name}</div>
+                <div>{room.recentMessage}</div>
+            </Paper>
+        </Link>
+    )
 }
