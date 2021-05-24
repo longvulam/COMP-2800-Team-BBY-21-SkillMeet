2c99aa13462bf4e6860f133265f8300c0a9704de
Lam Long Vu <longvulam91@gmail.com>
2021-05-24 15:03:58 -0700

    Connected friends pagewith chatrooms.

--- src/Friends/Friends.jsx
+++ src/Friends/Friends.jsx
@@ -11,55 +11,17 @@ export default function FriendsPage() {
     const [isLoadingData, setIsLoadingData] = useState(true);
     const [friendsList, setFriendsList] = useState([]);
 
-    function loadFriendsData(friendIds) {
-        db.collection('users')
-            .where(firebase.firestore.FieldPath.documentId(), 'in', friendIds)
-            .get()
-            .then(querySs => {
-                if (!querySs.empty) {
-                    const friendsData = querySs.docs.map(doc => doc.data());
-                    console.log(friendsData);
-                    setFriendsList(friendsData);
-                }
-                setIsLoadingData(false);
-            });
-    }
-
-    useEffect(
-        () => waitForCurrentUser()
-            .then((currentUser) => {
-                const friendsRef = db.collection('users').doc(currentUser.uid)
-                    .collection('Friends');
-
-                Promise.all([
-                    friendsRef
-                    .where('isPending', '==', false)
-                    .get()
-                    .then(querySs => {
-                        const arr = [];
-                        querySs.forEach(doc => arr.push(doc.data()));
-                        return arr;
-                    }),
-                    friendsRef.where('isConfirmed', '==', true)
-                    .get()
-                    .then(querySs => {
-                        const arr = [];
-                        querySs.forEach(doc => arr.push(doc.data()));
-                        return arr;
-                    })
-                ]).then(res => {
-                    const friends = [...res[0], ...res[1]];
-                    if (friends.length == 0) {
-                        setIsLoadingData(false);
-                        return;
-                    }
-                    const friendIds = friends.map(item => item.friendID);
-                    loadFriendsData(friendIds);
-                });
-            }).catch((err) => {
-                console.log(err);
-            })
-        , []);
+    useEffect(async () => {
+        const friends = await getAllFriendsOnUser();
+        if (friends.length == 0) {
+            setIsLoadingData(false);
+            return;
+        }
+        const friendIds = friends.map(item => item.friendID);
+        const profiles = await loadFriendsProfile(friendIds);
+        setFriendsList(profiles);
+        setIsLoadingData(false);
+    }, []);
 
     return (
         isLoadingData ? <LoadingSpinner /> :
@@ -81,7 +43,7 @@ export default function FriendsPage() {
                             alignItems: 'center',
                         }}>
                         {friendsList.map(friendInfo => {
-                            const { displayName, id } = friendInfo;
+                            const { displayName, id, chatRoomId } = friendInfo;
                             return (
                                 <Grid item
                                     xs={12}
@@ -89,7 +51,10 @@ export default function FriendsPage() {
                                     style={{
                                         width: '100%',
                                     }}>
-                                    <FriendCard friendId={id} name={displayName} />
+                                    <FriendCard
+                                        friendId={id}
+                                        chatRoomId={chatRoomId}
+                                        friendName={displayName} />
                                 </Grid>
                             );
                         })}
@@ -97,5 +62,47 @@ export default function FriendsPage() {
                 </div>
             </>
     );
+}
 
+const mapDocsToData = querySs => querySs.empty ? [] :
+    querySs.docs.map(doc => {
+        const data = doc.data();
+        data.id = doc.id;
+        return data;
+    });
+
+async function getAllFriendsOnUser() {
+    const currentUser = await waitForCurrentUser();
+    const friendsRef = db.collection('users').doc(currentUser.uid)
+        .collection('Friends');
+
+    const res = await Promise.all([
+        friendsRef.where('isPending', '==', false).get().then(mapDocsToData),
+        friendsRef.where('isConfirmed', '==', true).get().then(mapDocsToData)
+    ]);
+    const friends = [...res[0], ...res[1]];
+    return friends;
+}
+
+async function loadFriendsProfile(friendIds) {
+    const res = await Promise.all([
+        db.collection('users')
+            .where(firebase.firestore.FieldPath.documentId(), 'in', friendIds)
+            .get().then(mapDocsToData),
+        db.collection('chatrooms')
+            .where('uids', 'array-contains-any', friendIds)
+            .get().then(mapDocsToData)
+    ])
+
+    return matchProfilesToChatRooms(res);
+}
+
+function matchProfilesToChatRooms(results) {
+    const friendsProfile = results[0];
+    const chatRooms = results[1];
+    friendsProfile.forEach(profile => {
+        const chatroom = chatRooms.find(room => room.uids.find(uid => uid === profile.id));
+        profile.chatRoomId = chatroom ? chatroom.id : undefined;
+    });
+    return friendsProfile;
 }
--- src/Friends/friendsComponents/FriendCard.jsx
+++ src/Friends/friendsComponents/FriendCard.jsx
@@ -1,32 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MessageIcon from '@material-ui/icons/Message';
 import { useHistory } from 'react-router';
+import { db, getCurrentUserDataAsync } from '../../firebase';
 
 export default function FriendCard(props) {
     const history = useHistory();
-    const { friendId, name } = props;
+    const { friendId, friendName, chatRoomId } = props;
+
+    async function createChatroomAndRedirect() {
+        let roomId = chatRoomId;
+        if (!roomId) {
+            roomId = await createChatRoomInDB(friendId, friendName);
+        }
+
+        history.push('/chatRoom/' + roomId);
+    }
 
     return (
         <>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'space-around',
-                    alignItems: 'center',
-                    width: '95%',
-                    margin: 'auto',
-                }}>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                width: '95%',
+                margin: 'auto',
+            }}>
                 <Avatar
-                    onClick={()=>history.push('/profile/' + friendId)}
+                    onClick={() => history.push('/profile/' + friendId)}
                     style={{
                         width: '3em',
                         height: '3em',
                     }}
-                    alt={name} />
-
+                    alt={friendName}
+                />
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
@@ -36,13 +45,34 @@ export default function FriendCard(props) {
                             textAlign: 'center',
                             width: '30vw',
                         }}>
-                        {name}
+                        {friendName}
                     </Typography>
                 </div>
-                <IconButton>
+                <IconButton onClick={event => createChatroomAndRedirect(chatRoomId)}>
                     <MessageIcon />
                 </IconButton>
             </div>
         </>
     );
+}
+
+async function createChatRoomInDB(friendId, friendName) {
+    const currentUser = await getCurrentUserDataAsync();
+    const uid = currentUser.id;
+    const res = await db.collection('chatrooms').add({
+        recentMessage: "",
+        uids: [uid, friendId]
+    })
+    const roomId = res.id;
+    db.collection('users').doc(uid)
+        .collection('chatrooms').add({
+            name: friendName,
+            roomId
+        })
+    db.collection('users').doc(friendId)
+        .collection('chatrooms').add({
+            name: currentUser.displayName,
+            roomId
+        })
+    return roomId;
 }
