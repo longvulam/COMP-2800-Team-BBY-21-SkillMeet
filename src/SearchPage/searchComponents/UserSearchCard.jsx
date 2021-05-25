b6eeef35e28298fbca6d2ba327b22654ada19d66
Lam Long Vu <longvulam91@gmail.com>
2021-05-24 23:59:03 -0700

    Fix using the right firebase import.

--- src/SearchPage/searchComponents/UserSearchCard.jsx
+++ src/SearchPage/searchComponents/UserSearchCard.jsx
@@ -5,10 +5,12 @@ import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import firebase, { db, auth } from '../../firebase';
+import { db, auth } from '../../firebase';
 import Snackbar from '@material-ui/core/Snackbar';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import MuiAlert from '@material-ui/lab/Alert';
+
+import firebase from 'firebase';
 
 function Alert(props) {
   return <MuiAlert elevation={6} variant="filled" {...props} />;
