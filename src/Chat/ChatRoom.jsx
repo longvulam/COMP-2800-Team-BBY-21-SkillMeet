b7b83474ec79dc4e8c000c6e7fc06de3fbf82506
Lam Long Vu <longvulam91@gmail.com>
2021-05-24 20:22:11 -0700

    Add sorting to chatroom messages.

--- src/Chat/ChatRoom.jsx
+++ src/Chat/ChatRoom.jsx
@@ -1,64 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import Message from './chatPageComponents/message';
 import { auth, db, waitForCurrentUser } from '../firebase';
 import { Button, InputBase, Paper } from '@material-ui/core';
 import LoadingSpinner from '../classes/LoadingSpinner';
 
-let collRef = db.collection('chatrooms')
-    .doc()
-    .collection('messages');
-let chatroomRef = db.collection('chatrooms')
-    .doc();
-
-async function enableListening(updateMessages) {
-    collRef.onSnapshot(querySnapshot => {
-        const arr = [];
-        querySnapshot.docChanges().forEach(change => {
-            if (change.type === 'added') {
-                arr.push(change.doc.data());
-            }
-        });
-        updateMessages(oldArray => {
-            return [...oldArray, ...arr];
-        });
-    });
-}
-
-function setDbRef(chatRoomId) {
-    chatroomRef = db.collection('chatrooms').doc(chatRoomId);
-    collRef = chatroomRef.collection('messages');
-}
-
-/** @param {String} newMessage */
-function sendMessageToDB(newMessage) {
-    
-    if(newMessage.length === 0) return;
-
-    newMessage = newMessage.replace('\n', '\\n');
-    collRef.add({
-        from: auth.currentUser.uid,
-        content: newMessage,
-        timeStamp: new Date().getTime()
-    });
-    const recentMessage = newMessage.length > 40 ? newMessage.slice(0, 40) + "..." : newMessage;
-    chatroomRef.set({
-        recentMessage: recentMessage
-    }, { merge: true });
-}
-
-async function getChatRoomName(roomId) {
-    const currentUser = await waitForCurrentUser();
-    const userChatRoom = await db.collection('users').doc(currentUser.uid)
-    .collection('chatrooms').where('roomId', '==', roomId).get();
-    return userChatRoom.docs[0].data().name;
-}
-
-const ChatRoom = () => {
+export default function ChatRoom(props) {
     const { chatRoomId } = useParams();
     const [isLoading, setIsLoading] = useState(true);
     const [messages, updateMessages] = useState([]);
-    const [currentMsg, setCurrentMsg] = useState([]);
+    const [currentMsg, setCurrentMsg] = useState("");
     const [chatRoomName, setChatRoomName] = useState("");
 
     useEffect(async () => {
@@ -107,8 +58,7 @@ const ChatRoom = () => {
                     onChange={handleChange}
                     onKeyPress={submitOnCtrlEnter}
                 />
-                <Button onClick={sendMessage}
-                >Send message</Button>
+                <Button onClick={sendMessage}>Send message</Button>
             </Paper>
         </div>
     );
@@ -128,4 +78,52 @@ const styles = {
     }
 }
 
-export default ChatRoom;
+let collRef = db.collection('chatrooms')
+    .doc()
+    .collection('messages');
+let chatroomRef = db.collection('chatrooms')
+    .doc();
+
+async function enableListening(updateMessages) {
+    collRef.onSnapshot(querySnapshot => {
+        const arr = [];
+        querySnapshot.docChanges().forEach(change => {
+            if (change.type === 'added') {
+                arr.push(change.doc.data());
+            }
+        });
+        arr.sort((a,b) => a.timeStamp > b.timeStamp ? 1 : -1);
+        updateMessages(oldArray => {
+            return [...oldArray, ...arr];
+        });
+    });
+}
+
+function setDbRef(chatRoomId) {
+    chatroomRef = db.collection('chatrooms').doc(chatRoomId);
+    collRef = chatroomRef.collection('messages');
+}
+
+/** @param {String} newMessage */
+function sendMessageToDB(newMessage) {
+    
+    if(newMessage.length === 0) return;
+
+    newMessage = newMessage.replace('\n', '\\n');
+    collRef.add({
+        from: auth.currentUser.uid,
+        content: newMessage,
+        timeStamp: new Date().getTime()
+    });
+    const recentMessage = newMessage.length > 40 ? newMessage.slice(0, 40) + "..." : newMessage;
+    chatroomRef.set({
+        recentMessage: recentMessage
+    }, { merge: true });
+}
+
+async function getChatRoomName(roomId) {
+    const currentUser = await waitForCurrentUser();
+    const userChatRoom = await db.collection('users').doc(currentUser.uid)
+    .collection('chatrooms').where('roomId', '==', roomId).get();
+    return userChatRoom.docs[0].data().name;
+}
