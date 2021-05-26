1cfb34b0fab92d78f8d885d219172173a9a35c58
Dustin Lott <dustin.lott93@gmail.com>
2021-05-26 12:56:54 -0700

    added styling to chatroom page.

--- src/Chat/chatPageComponents/message.jsx
+++ src/Chat/chatPageComponents/message.jsx
@@ -1,25 +1,43 @@
 import { Fragment } from "react";
 import { auth } from "../../firebase";
+import { makeStyles } from '@material-ui/core/styles';
+import { PlayCircleFilledWhiteTwoTone } from "@material-ui/icons";
+import { Button, IconButton, InputBase, Paper } from '@material-ui/core';
 
-const styles = {
-    message: {
-        paddingTop: '14px'
+const useStyles = makeStyles((theme) => ({
+    
+    currentUserStyle: {
+        padding: '10px 7px 10px 7px',
+        textAlign: 'right',
+        marginRight: '.5em',
+        marginLeft: '30vw',
     },
-    date: {
-        paddingTop: '7px',
+    currentUserDateStyle: {
+        textAlign: 'right',
         fontSize: 'small',
+        paddingTop: '7px',
         color: 'slategrey',
-    }
-}
+        marginRight: '.6em',
+        marginTop: '-.4em',
+    },
+    otherUserStyle: {
+        padding: '10px 7px 10px 7px',
+        textAlign: 'left',
+        backgroundColor: 'blue',
+        color: 'white',
+        width: '70vw',
+        marginLeft: '.5em',
+    },
+    otherUserDateStyle: {
+        textAlign: 'left',
+        fontSize: 'small',
+        paddingTop: '7px',
+        color: 'slategrey',
+        marginLeft: '.6em',
+        marginTop: '-.4em',
+    },
+}));
 
-const currentUserStyle= {
-    ...styles.message,
-    textAlign: 'right'
-}
-const otherUserStyle = {
-    ...styles.message,
-    textAlign: 'left'
-}
 
 function format(date) {
     let formattedDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
@@ -29,11 +47,13 @@ function format(date) {
 
 const Message = (props) => {
     const { content, from, timeStamp } = props;
+    const classes = useStyles();
 
     const dateStr = format(new Date(timeStamp));
     const split = content.split("\\n");
     return (
-        <div style={from === auth.currentUser.uid ? currentUserStyle : otherUserStyle}>
+        <Fragment>
+        <Paper className={from === auth.currentUser.uid ? classes.currentUserStyle : classes.otherUserStyle}>
             {split.map((value, index) => {
                 return (
                     <Fragment key={index}>
@@ -42,8 +62,9 @@ const Message = (props) => {
                     </Fragment>
                 );
             })}
-            <div style={styles.date}>{dateStr}</div>
-        </div>
+        </Paper>
+        <div className={from === auth.currentUser.uid ? classes.currentUserDateStyle : classes.otherUserDateStyle}>{dateStr}</div>
+        </Fragment>
     );
 }
 
