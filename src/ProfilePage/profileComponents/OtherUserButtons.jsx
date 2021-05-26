c087dc15e0094b9ce39a35c5675c343c9b191306
Lam Long Vu <longvulam91@gmail.com>
2021-05-25 21:23:04 -0700

    Add sending friend request & connect to chatroom.

--- src/ProfilePage/profileComponents/OtherUserButtons.jsx
+++ src/ProfilePage/profileComponents/OtherUserButtons.jsx
@@ -4,30 +4,67 @@ import PersonAddIcon from '@material-ui/icons/PersonAdd';
 import MessageIcon from '@material-ui/icons/Message';
 import { useStyles } from '../Profile';
 import { db, firestore, getCurrentUserDataAsync } from '../../firebase';
+import { useHistory } from 'react-router';
 
 export function OtherUserButtons(props) {
-    const { isFriend, isPending, friendId } = props;
-    const [isPendingFriendShip, setIsPendingFriendShip] = useState(isPending);
+    const history = useHistory();
+    const { profileData, isFriend, setLoading } = props;
+    const [isPendingFriendShip, setIsPendingFriendShip] = useState(profileData.isPending);
     const classes = useStyles();
 
     async function handleClick(e) {
-        addFriend(friendId);
+        addFriend(profileData.id);
         setIsPendingFriendShip(true);
+    }
+
+    async function createChatroomAndRedirect(e) {
+        setLoading(true);
+        try {
+            let roomId = profileData.chatRoomId;
+            if (!roomId) {
+                roomId = await createChatRoomInDB(profileData.id, profileData.displayName);
+            }
+
+            history.push('/chatRoom/' + roomId);
+        } catch (error) {
+            setLoading(false);
+        }
     }
 
     return (
         <div className={classes.buttonsWrap}>
             {isFriend ?
-                (<IconButton>
+                (<IconButton onClick={createChatroomAndRedirect}>
                     <MessageIcon />
-                </IconButton>
-                ) :
-                (<IconButton 
-                  onClick={handleClick}
-                  disabled={isPendingFriendShip}>
+                </IconButton>) :
+                (<IconButton
+                    onClick={handleClick}
+                    disabled={isPendingFriendShip}>
                     <PersonAddIcon />
                 </IconButton>)}
         </div>);
+}
+
+async function createChatRoomInDB(friendId, friendName) {
+    const currentUser = await getCurrentUserDataAsync();
+    const uids = [currentUser.id, friendId];
+    const res = await db.collection('chatrooms').add({
+        recentMessage: "",
+        uids
+    });
+
+    const roomId = res.id;
+    db.collection('users').doc(currentUser.id)
+        .collection('chatrooms').add({
+            name: friendName,
+            roomId
+        })
+    db.collection('users').doc(friendId)
+        .collection('chatrooms').add({
+            name: currentUser.displayName,
+            roomId
+        })
+    return roomId;
 }
 
 async function addFriend(uid) {
