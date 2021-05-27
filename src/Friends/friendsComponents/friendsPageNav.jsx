ad1d17f6f28c207e626ee880c67880251f5e08aa
Lam Long Vu <longvulam91@gmail.com>
2021-05-26 17:05:20 -0700

    Removed group tab. Changed friends tab icon to group. Add badges for friend requests.

--- src/Friends/friendsComponents/friendsPageNav.jsx
+++ src/Friends/friendsComponents/friendsPageNav.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FriendIcon from '@material-ui/icons/Person';
 import GroupIcon from '@material-ui/icons/Group';
 import Pending from '@material-ui/icons/PersonAdd';
 
 import { useHistory } from 'react-router-dom';
+import { db, waitForCurrentUser } from '../../firebase';
+import { Badge } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -26,6 +27,9 @@ export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState('recents');
   const history = useHistory();
   const [historyValue, setHistoryValue] = React.useState("");
+  const [newRequestsNo, setNewRequestsNo] = React.useState(0);
+
+  React.useEffect(async ()=> subscribeToUpdates(setNewRequestsNo), []);
 
   function handleChange(event, newValue){
     setValue(newValue);
@@ -39,11 +43,21 @@ export default function LabelBottomNavigation() {
 
   return (
     <div className = {classes.navbarWrap}>
-    <BottomNavigation value={value} onChange={twoCallbacks} className={classes.root}>
-      <BottomNavigationAction label="Friends" value="/friends" icon={<FriendIcon />} />
-      <BottomNavigationAction disabled label="Groups" value="/groups" icon={<GroupIcon style={{color:'lightgrey'}}/>} />
-      <BottomNavigationAction label="Requests" value="/friendRequests" icon={<Pending/>} />
-    </BottomNavigation>
+      <BottomNavigation value={value} onChange={twoCallbacks} className={classes.root}>
+        <BottomNavigationAction label="Friends" value="/friends" 
+          icon={
+            <Badge badgeContent={newRequestsNo} color="error">
+              <GroupIcon />
+            </Badge>} 
+        />
+        <BottomNavigationAction label="Requests" value="/friendRequests" icon={<Pending/>} />
+      </BottomNavigation>
     </div>
   );
+}
+
+async function subscribeToUpdates(setNewRequestsNo){
+    const user = await waitForCurrentUser();
+    db.collection('users').doc(user.uid)
+      .onSnapshot(doc => setNewRequestsNo(doc.data().newRequestsNo));
 }
