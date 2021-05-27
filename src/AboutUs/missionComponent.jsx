10c60eabf94ed2cbb33d4b8bf9f4032a42e7e488
Dustin Lott <dustin.lott93@gmail.com>
2021-05-27 09:36:00 -0700

    update mission

--- src/AboutUs/missionComponent.jsx
+++ src/AboutUs/missionComponent.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Jumbotron, Button } from 'react-bootstrap';
+import Container from '@material-ui/core/Container';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class Mission extends Component {
     render() {
-        return <div><Container style={{ textAlign: 'center', border: '1px solid black', marginTop: '3rem', backgroundColor: 'black' }}>
-            <h1 style={{ color: 'goldenrod', marginTop: '1.5rem' }}>Our Mission..</h1>
-            <p style={{ marginBottom: '1.5rem', color: 'white' }}>"At SkillMeet our mission is to enable people to connect and grow by create a space where like-minded individuals gather to inspire one another."</p>
-
-        </Container>
-            <div style={{ textAlign: 'center' }}>
-                <FontAwesomeIcon icon={["fas", "chevron-down"]} color="black" opacity=".4" size="3x" onClick={() => window.scrollBy(0, 820)} />
+        return <React.Fragment>
+        <div style={{ textAlign: 'center', border: '1px solid black', marginTop: '3rem', backgroundColor: 'black', marginBottom: '0rem' }}>
+            <h1 style={{ color: 'lightblue', marginTop: '1.5rem' }}>Our Mission..</h1>
+            <p style={{ marginBottom:'1.5rem', color: 'white' }}>"At SkillMeet our mission is to enable people to connect and grow by create a space where like-minded individuals gather to inspire one another."</p>
             </div>
-        </div>
+            <div style={{ textAlign: 'center',}}>
+                <FontAwesomeIcon icon={["fas", "chevron-down"]} color="black" opacity=".4" size="3x" onClick={() => window.scrollBy(0, 690)} />
+            </div>
+            </React.Fragment>
             ;
     }
 }
