45db881c1ec79812070baed5096ed0203bec8a4c
Lam Long Vu <longvulam91@gmail.com>
2021-05-27 15:03:23 -0700

    Restructure Profile page components.

--- src/ProfilePage/Profile.jsx
+++ src/ProfilePage/Profile.jsx
@@ -8,9 +8,9 @@ import EditButton from './profileComponents/ProfileEditButton';
 import FacebookBtn from './profileComponents/ProfileFacebookButton';
 import LogOutButton from './profileComponents/LogOutButton';
 import { OtherUserButtons } from './profileComponents/OtherUserButtons';
-import { ProfileAlert } from './profileComponents/profileAlert';
+import { ProfileAlert } from './profileComponents/ProfileAlert';
 import { PersonalInfo } from './profileComponents/PersonalInfo';
-import { SkillsList } from './SkillsList';
+import { SkillsList } from './profileComponents/SkillsList';
 
 import LoadingSpinner from '../common/LoadingSpinner';
 import { db, getCurrentUserDataAsync, waitForCurrentUser } from '../firebase';
