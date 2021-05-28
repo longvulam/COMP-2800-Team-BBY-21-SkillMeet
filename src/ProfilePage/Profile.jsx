5196e2f844afbd5fadf758592d8643c0ee062836
Lam Long Vu <longvulam91@gmail.com>
2021-05-27 17:44:45 -0700

    Fix routes.

--- src/ProfilePage/Profile.jsx
+++ src/ProfilePage/Profile.jsx
@@ -9,7 +9,7 @@ import FacebookBtn from './profileComponents/ProfileFacebookButton';
 import TwitterBtn from './profileComponents/ProfileTwitterButton';
 import LogOutButton from './profileComponents/LogOutButton';
 import { OtherUserButtons } from './profileComponents/OtherUserButtons';
-import { ProfileAlert } from './profileComponents/profileAlert';
+import { ProfileAlert } from './profileComponents/ProfileAlert';
 import { PersonalInfo } from './profileComponents/PersonalInfo';
 import { SkillsList } from './profileComponents/SkillsList';
 
