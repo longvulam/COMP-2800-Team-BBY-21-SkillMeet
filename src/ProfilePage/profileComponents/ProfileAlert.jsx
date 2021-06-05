d6ce47c109f6732873779203d7bdc43e6ab908f8
Lam Long Vu <longvulam91@gmail.com>
2021-06-05 11:31:59 -0700

    Correct typo in ProfileAlert.

--- src/ProfilePage/profileComponents/ProfileAlert.jsx
+++ src/ProfilePage/profileComponents/ProfileAlert.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MuiAlert from '@material-ui/lab/Alert';
 
 /**
- * Creats a Mui alert based on what is passed through the props
+ * Creates a Mui alert based on what is passed through the props
  * @param {*} props props passed into the function.
  * @returns an alert with the props
  */
